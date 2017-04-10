0ca52b1e892903bd0d41c97616ab4d13c65eda1c
Albert Carter <albert.r.carter@gmail.com>
2017-04-10 16:51:47 -0400

    add white space to rerun

--- server/start_server.sh
+++ server/start_server.sh
@@ -5,6 +5,7 @@
 # check if thrift version specified and pass that on
 THRIFT_VERSION="-Dthrift_version=0.9.3"
 
+
 # echo "mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass=\"edu.mit.csail.db.ml.main.Main\" $THRIFT_VERSION"
 mvn clean compile $THRIFT_VERSION
 mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" $THRIFT_VERSION
