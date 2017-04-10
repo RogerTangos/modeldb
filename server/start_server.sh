378e7b97ea82fb679887ef7eeacdaef60bf99786
Albert Carter <albert.r.carter@gmail.com>
2017-04-10 16:31:41 -0400

    hardcode thrift version

--- server/start_server.sh
+++ server/start_server.sh
@@ -3,9 +3,8 @@
 ../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/'
 
 # check if thrift version specified and pass that on
-if [ -n "$1" ]; then
-	THRIFT_VERSION="-Dthrift_version=$1"
-fi
+THRIFT_VERSION="-Dthrift_version=0.9.3"
+
 # echo "mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass=\"edu.mit.csail.db.ml.main.Main\" $THRIFT_VERSION"
 mvn clean compile $THRIFT_VERSION
 mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" $THRIFT_VERSION
