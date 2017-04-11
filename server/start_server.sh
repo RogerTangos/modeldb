687a1eeb6fad41f413290c3352d55e3cd92c9846
Albert Carter <albert.r.carter@gmail.com>
2017-04-11 16:01:53 -0400

    rm shebang. put ss lines together with &&

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # generate thrift file
 ../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/'
 
@@ -7,5 +6,4 @@ if [ -n "$1" ]; then
 	THRIFT_VERSION="-Dthrift_version=$1"
 fi
 # echo "mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass=\"edu.mit.csail.db.ml.main.Main\" $THRIFT_VERSION"
-mvn clean compile $THRIFT_VERSION
-mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" $THRIFT_VERSION &
+mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" $THRIFT_VERSION &
