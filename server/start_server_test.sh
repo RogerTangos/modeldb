1ff8d2c3143d6725e10e3492e0550b1c47838f83
Albert Carter <albert.r.carter@gmail.com>
2017-04-18 15:50:14 -0400

    move test args into start_server_test, and use that

--- server/start_server_test.sh
+++ server/start_server_test.sh
@@ -1,2 +1,21 @@
-../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/' 
-mvn clean compile && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf"
+#!/usr/bin/env bash
+# generate thrift file
+../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/'
+
+# check if thrift version specified and pass that on
+if [ -n "$1" ]; then
+    THRIFT_VERSION="-Dthrift_version=$1"
+fi
+# echo "mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass=\"edu.mit.csail.db.ml.main.Main\" $THRIFT_VERSION"
+
+# The next two lines of code must be seperate (not joined by &&)
+# for Travis continuous integration tests to run
+mvn clean compile $THRIFT_VERSION
+mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf" $THRIFT_VERSION &
+
+
+
+
+# mvn clean compile && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf"
+
+# -Dtest=TestFitEvent -Dthrift_version=0.9.3
